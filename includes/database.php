57f452ac540d5ea895df34717e349c63cf24fe03
Leny WV <lenywv@gmail.com>
2015-05-01 22:02:28 +0530

    db credentials removed

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,11 @@
 <?php
-	$con= mysqli_connect("localhost","root","root"); 
+	require_once("../includes/credentials.php");
+	$con= mysqli_connect($db_serv,$db_user,$db_pass); 
 	if (!$con) { 
 		die('Could not connect to MySQL: ' ); 
 	} 
 	else
 	{
-		mysqli_select_db($con,"Book_Recommendation");
+		mysqli_select_db($con,$db_name);
 	}
 ?>
