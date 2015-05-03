69bdd963961f07de2c52a37c227256559cf0c41a
kPrasadk <prasadkk940@gmail.com>
2015-05-04 02:37:02 +0530

    Added user validation

--- homepage.php
+++ homepage.php
@@ -6,15 +6,28 @@
 					FROM User 
 					WHERE UserID={$User['userid']},Email={$User['email']}";
 		$queryStatus=mysqli_query($con,$sql_query);
+
 		if(mysqli_fetch_array($queryStatus)==false)
 		{
+			$sql_num="SELECT *
+				       FROM User";
+			$numrows=mysqli_num_rows(mysqli_query($con,$sql_num))+1;
 			$sql_insert="INSERT INTO User
-						 VALUES(1,{$User['userid']},{$User['email']},0) ";
+						 VALUES($numrows,{$User['userid']},{$User['email']},0) ";
 			$queryStatus=mysqli_query($con,$sql_query);
 			if($queryStatus==false)
 				echo "Some error has occured...\n";
 			else
 				echo "User inserted successfully....\n";
+		}
+		else
+		{
+			$var=mysqli_fetch_row($queryStatus);
+			if($var[3]==1)
+			{
+				//todo
+			}	
+
 		}			
 	}
 ?>
